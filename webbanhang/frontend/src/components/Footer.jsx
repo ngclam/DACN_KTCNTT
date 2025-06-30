9cd48f380d94fe41e066f2ab3655ebc11f342c93
khoiine <tkgamer142@gmail.com>
2025-06-30 15:38:28 +0700

    update

--- webbanhang/frontend/src/components/Footer.jsx
+++ webbanhang/frontend/src/components/Footer.jsx
@@ -33,7 +33,7 @@ const Footer = () => {
       </div>
       <div>
         <hr />
-        <p className='py-5 text-sm text-center'>Copyright 2024@ e-commerce.com - All Right Reserved.</p>
+        <p className='py-5 text-sm text-center'>Copyright 2025@ e-commerce.com - All Right Reserved.</p>
       </div>
     </div>
   )
